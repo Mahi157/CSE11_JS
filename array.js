74146d9fa33b77ebd3994e4f78d6c7b4ce3de2e7
Mahi Arora <mahiarora968@gmail.com>
2026-03-10 09:37:11 +0530

    array

---
+++ array.js
@@ -0,0 +1,18 @@
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    90009898193,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection.teacherName= "John Mac";
+collection.phoneNo = 950008193;
+
+var trainerinfo =["Jitender",29,"Ghaziabad",{isMarried:true}];
+console.log(trainerinfo);
--- arrow function.js
+++ arrow function.js
@@ -1,1 +1,4 @@
 var add3 =(a,b) => console.log(a+b);
+ var myinfo = new Array();
+var myinfo1 = Array();
+console.log(myinfo,myinfo1);
