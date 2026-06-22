af06d0dbcf1afc3a3f5a3b6848d800bab250c185
Mahi Arora <mahiarora968@gmail.com>
2026-06-22 15:43:36 +0530

    Mouse Hover Effects

--- DOM.js
+++ DOM.js
@@ -72,7 +72,7 @@
  var text ='{"employees":['+'{"firstName":"John","lastName":"Doe"},'
  +'{"firstName":"Anna","lastName":"Smith"},'+'{"firstName":"Peter","lastName":"Jones"}]}';
 
- obj=JSON.parse(text);
+//  obj=JSON.parse(text);
  document.getElementById("demo").innerHTML=obj.employees[1].firstName+"" +obj.employees[1].lastName;
 
  test.style.backgroundColor = "green";
